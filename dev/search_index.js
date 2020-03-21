6e04ed5b65238933a92d9df39b6f90eef5315ac2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-21 20:48:16 +0000

    build based on 8183729

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GuidedProposals.jl-1","page":"Home","title":"GuidedProposals.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"test documentation","category":"page"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [GuidedProposals]","category":"page"}]
+}
